53c537c0dddcb1d85a8321f4630bfd47d812f51a
Khoa Nguyen <khoanguyenx@icloud.com>
2025-11-22 23:51:18 +0700

    fix build errors

--- main.cpp
+++ main.cpp
@@ -13,41 +13,40 @@ static SDL_Renderer *renderer = nullptr;
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
   if (!SDL_CreateWindowAndRenderer("title", WINDOW_WIDTH, WINDOW_HEIGHT,
                                    SDL_WINDOW_RESIZABLE, &window, &renderer)) {
-      SDL_Log("could not create window and renderer, %s", SDL_GetError());
-      return SDL_APP_FAILURE;
+    SDL_Log("could not create window and renderer, %s", SDL_GetError());
+    return SDL_APP_FAILURE;
   }
   return SDL_APP_CONTINUE;
 }
 
 // run once per frame
-SDL_AppResult SDL_AppIterate(void **appstate) {
-    const std::string message = "Hello Chuc beo";
-    int w = 0, h = 0;
-    float x, y;
-    const float scale = 4.0f;
+SDL_AppResult SDL_AppIterate(void *appstate) {
+  const std::string message = "Hello Chuc beo";
+  int w = 0, h = 0;
+  float x, y;
+  const float scale = 4.0f;
 
-    SDL_GetRenderOutputSize(renderer, &w, &h);
-    SDL_SetRenderScale(renderer, scale, scale);
-    x = ((w / scale) - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE * message.length()) /
-        2;
-    y = ((h / scale) - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE) / 2;
+  SDL_GetRenderOutputSize(renderer, &w, &h);
+  SDL_SetRenderScale(renderer, scale, scale);
+  x = ((w / scale) - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE * message.length()) / 2;
+  y = ((h / scale) - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE) / 2;
 
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_RenderDebugText(renderer, x, y, message.c_str());
-    SDL_RenderPresent(renderer);
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+  SDL_RenderClear(renderer);
+  SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+  SDL_RenderDebugText(renderer, x, y, message.c_str());
+  SDL_RenderPresent(renderer);
 
-    return SDL_APP_CONTINUE;
+  return SDL_APP_CONTINUE;
 }
 
-SDL_AppResult SDL_AppEvent(void **appstate, SDL_Event *event) {
-    return SDL_APP_CONTINUE;
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
+  return SDL_APP_CONTINUE;
 }
 
-void SDL_AppQuit(void **appstate, SDL_AppResult result) {
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Log("app quit successfully");
-    SDL_Quit();
+void SDL_AppQuit(void *appstate, SDL_AppResult result) {
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Log("app quit successfully");
+  SDL_Quit();
 }
